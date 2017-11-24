2d0d581f5e97273c7cb73075fb18cb3318839d93
Alon <alontalmor@gmail.com>
2017-11-24 09:13:13 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', err => console.log('Unable to append to log file!'));
+    next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs',{
+//         pageTitle: 'Maintenance',
+//         maintanenceMessage: `We are currently under maintenance...`
+//     });
+// });
+
+app.use(express.static(__dirname + '/public')); //register middleware
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs',{
+        pageTitle: 'Home',
+        welcomeMessage: `Welcome to My Site!`
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs',{
+        pageTitle: 'About',
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        error: 'Error handling request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up...')
+});
